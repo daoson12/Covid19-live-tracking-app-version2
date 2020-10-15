2f4872a51120bfb3b952c987d217b48186e25a9f
daoson12 <daoduvictor48@gmail.com>
2020-10-15 11:30:35 -0700

    update

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -8,8 +8,8 @@ import { DateWiseData } from './../components/countries/models/date-wise-data';
 })
 export class DataServiceService {
  
-  private globalDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/';
-  private dateWiseDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
+  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-17-2020.csv`;
+  private dateWiseDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv`
   constructor(private http: HttpClient) { }
 
 
